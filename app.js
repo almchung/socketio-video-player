512ed77291764739c00446f1faedb4a0f843b4a2
almchung <almchung@ucla.edu>
2017-05-26 17:16:55 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+var connectCounter = 0;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static('public'))
+app.use(express.static('files'))
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
+io.sockets.on('connection', function(client){
+  client.emit('broadcast', { description: ' connected!'});
+  
+  client.on('join', function(data){
+    console.log(data);
+    client.emit('broadcast', { description: data});
+  });
+  
+  client.on('button-pushed', function(data){
+    console.log("The puppet-master pushed the big play button!");
+    client.emit('play');
+    io.sockets.emit('play');
+  });
+});
+
+    /*var Room = "";
+    client.on("setNickAndRoom", function(nick, fn){
+        client.join(nick.room);
+        Room = nick.room;
+        client.broadcast.to(Room).emit('count', "Connected:" + " " + count);
+        fn({msg :"Connected:" + " " + count});
+    });*/
+
+
+
+http.listen(3000, function(){
+  console.log('listening on localhost:3000');
+});
---
+++ public/p5js_video.js
@@ -0,0 +1,29 @@
+var playing = false;
+var videos;
+
+function setup() {
+  //createCanvas(1920, 1080);
+  // specify multiple formats for different browsers
+  //videos = createVideo("/assets/sample.mov");
+  //videos.hide();
+}
+
+function draw(){
+  //background(150);
+  //image(videos,0,0);
+}
+
+// plays or pauses the video depending on current state
+/*function toggleVid() {
+  if (playing) {
+    videos.pause();
+  } else {
+    videos.loop();
+  }
+  playing = !playing;
+}*/
+
+function playVid(){
+  //videos.loop();
+  console.log("VIDEO PLAYING!!!!");
+}

e35332e953fe6afab7c6bce6c1bfee2f97c05312
almchung <almchung@ucla.edu>
2017-05-26 20:29:30 -0700

    PORT setting

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var io = require('socket.io');
+var socketIO = require('socket.io');
 var path = require('path');
 
 var connectCounter = 0;
@@ -8,12 +8,14 @@ const PORT = process.env.PORT || 8877;
 const INDEX = __dirname + '/index.html';
 
 var app = express()
-  .use((req, res) => res.sendFile(INDEX))
+  .get('/', function(req, res){  res.sendFile(__dirname + '/index.html');
+})
+  .use(express.static('public'))
+  .use(express.static('files'))
+  .use('/public', express.static(path.join(__dirname, 'public')))
   .listen(PORT, () => console.log('Listening on ${ PORT }'));
 
-app.use(express.static('public'))
-app.use(express.static('files'))
-app.use('/public', express.static(path.join(__dirname, 'public')))
+const io = socketIO(app);
 
 io.sockets.on('connection', function(client){
   client.emit('broadcast', { description: ' connected!'});
